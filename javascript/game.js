04bfe8380be728acebcec88ed0e6288edce5f26f
burdagol5000 <vicenteponteras@gmail.com>
2022-12-31 15:28:53 +0000

    scripting for choosing random word from an array

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,11 @@
 // words to be chosen randomly
 var normal=["elephant","zebra","canada","europe","asia","america","blue","black","violet","panda"]
-
+// variables to store the word chosen and the mistakes guessed and the correct guess limiter
 var word='';
+let limit = 10;
+let mistakes = 0;
+let correct = [];
+
+function random() {
+    answer = normal[Math.floor(Math.random() * normal.length)];
+  }
