fd84d7913eb4075a19998a3292f48a7339638f7b
burdagol5000 <vicenteponteras@gmail.com>
2022-12-31 18:02:15 +0000

    added comments on game.js

--- javascript/game.js
+++ javascript/game.js
@@ -5,51 +5,57 @@ var word='';
 let chance = 5;
 let correct = [];
 words=null;
+
 //function to generate a random word from the array
 function random() {
     word = normal[Math.floor(Math.random() * normal.length)];
 }
+
 //function to generate the buttons to be used to play the game
 function Buttons() {
-    let buttons = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
-        `
-        <button id='` + letter + `'onClick="Guess('` + letter + `')">
-        ` + letter + `
-        </button>
-        `).join('');
+    let buttons = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>   
+        `<button id='` + letter + `'onClick="Guess('` + letter + `')"> ` + letter + `</button> `).join(''); 
     document.getElementById('letters').innerHTML = buttons;
 }
 
+//function that will generate the spaces where letters will appear if successfully guessed
 function problem() {
     words = word.split('').map(letter => (correct.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-  
     document.getElementById('problem').innerHTML = words;
 }
-
+//function to check if the pushed/clicked button is on the random word that was chosen
 function Guess(chosen) {
     correct.indexOf(chosen) === -1 ? correct.push(chosen) : null;
     document.getElementById(chosen).setAttribute('disabled', true);
-  
     if (word.indexOf(chosen) >= 0) {
+
+    //if clicked letter is on the random word problem() function will be ran again to update the shown and hidden letters on h3 problem
       problem();
     } else if (word.indexOf(chosen) === -1) {
-      chance--;
-      chances();
-      zerochances()
-    }
-  }
 
-  function chances() {
+    //else if the clicked button/letter is not on the random word, deduct the number of chances,execute chances() function and zerochances() function
+    chance--;
+    chances();
+    zerochances()
+    }
+}
+
+//function that will update the span chance on how many chances left
+function chances() {
     document.getElementById('chances').innerHTML = chance;
-  }
-  function zerochances() {
+}
+
+//function that will check if chance reaches 0, then player loses the game
+function zerochances() {
     if (chance === 0) { 
         document.getElementById('bombs').src = 'assets/nuke.jpg';
       document.getElementById('letters').innerHTML = '<a href="index.html">Back</a>';
       
     }
-  }
-  
+}
+
+
+
 random();
 Buttons();
 problem();
