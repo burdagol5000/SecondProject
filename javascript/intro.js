d5c59031c0556b414e0ea804934ebab95ae2c77a
burdagol5000 <vicenteponteras@gmail.com>
2022-12-31 11:08:50 +0000

    solved the double click for onclick event

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,8 +1,9 @@
 function difficulty() {
     var appear = document.getElementById("levels");
-    var start = document.getElementById("start");
-    if (appear.style.display === "none") {
-        appear.style.display = "block"; 
+    var start= document.getElementById("start");
+    if (appear.style.display === "none"||start.style.display==="block") {
+        appear.style.display = "block";
+        start.style.display = "none";
     } else {
         appear.style.display = "none";
     }
